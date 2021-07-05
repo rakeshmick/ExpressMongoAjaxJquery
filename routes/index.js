8aaa2171b82e6c51c7672fd503d88fc0b5d7ba1d
Rakesh Prabhakaran <rakesh.1985march@gmail.com>
2021-07-05 20:23:23 +1000

    serving index.html from router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname,"/../views/index.html"));
 });
 
-module.exports = router;
+module.exports = router
